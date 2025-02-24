853fe23587522b51dec784c1faefabef798b88e5
Mayur-Tri <mayurshimpi.tri@gmail.com>
2025-02-24 09:31:54 +0530

    hero video update

--- src/components/landing/heroSection/index.tsx
+++ src/components/landing/heroSection/index.tsx
@@ -1,9 +1,10 @@
+
 const Hero = () => {
     return (
         <section className="relative w-full h-screen overflow-hidden text-center">
             <video
                 className="absolute top-0 left-0 w-full h-full object-cover"
-                src="https://s3-figma-videos-production-sig.figma.com/video/1427644921295469162/TEAM/ac07/589f/-3105-4bcf-bba2-55b3eb98c6b5?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Y~eiWwQZ~es8mAvrp6cQ6OU8lYQi8fTzutL~llIp71JV3Pv7GMzQ3XLWM8lHG~SRmUoxrRKbVq4~Pxgnhw55QVZ-3F~bDNaAFUlGiidD2w6lGpGsP9uiHO9uHUg~xaLjc4m-f7Zjro0yW~YRpfbSfOs-D9alKJo0fqQB~Qi-XhCAArgYdlAZXd58tY91Iz7y6de0DbO8uR5aPMwixfLAYSkiT-3vMsfjYKbygA~F~s6u1L1mF03lFLn~MoE6ZfL8jlPrm-PSoaFagNUkSv3t6zd6I~HYsN4RPyp5Xds6y7XAEaKxb-PsTZoGxxXVKTMRL~kFNLo9~g~WvgNlXcRE4A__"
+                src="/assets/videos/hero.mp4"
                 autoPlay
                 loop
                 muted
