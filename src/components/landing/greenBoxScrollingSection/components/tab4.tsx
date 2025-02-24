bd12c0a1458d9af527c91cab0633ed6b408a730c
Mayur-Tri <mayurshimpi.tri@gmail.com>
2025-02-24 10:15:05 +0530

    greenbox animation update

--- src/components/landing/greenBoxScrollingSection/components/tab4.tsx
+++ src/components/landing/greenBoxScrollingSection/components/tab4.tsx
@@ -1,9 +1,27 @@
+import { motion } from "framer-motion";
+
 const Tab4 = () => {
+    const fadeUpZoomInVariants = {
+        hidden: { opacity: 0, y: 50, scale: 0.8 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            scale: 1,
+            transition: { duration: 0.8, ease: "easeOut" }
+        },
+    };
     return (
         <div className='text-center'>
             <div className='grid grid-cols-1 lg:grid-cols-3 items-center'>
                 <div className='text-center col-span-2 '>
-                    <img src="/assets/svg/greenhouse-box-state-4.svg" alt="Greenhouse Box" className="mx-auto w-[70%] lg:w-[65%]"/>
+                    <motion.div
+                        variants={fadeUpZoomInVariants}
+                        initial="hidden"
+                        animate="visible"
+                        className="w-full "
+                    >
+                        <img src="/assets/svg/greenhouse-box-state-3.svg" alt="Greenhouse Box" className="mx-auto w-[70%] lg:w-[65%]" />
+                    </motion.div>
                 </div>
                 <div className='flex justify-center mt-10 lg:mt-0'>
                     <ul className='text-left w-[80%] lg:w-full justify-content-center lg:justify-content-left p-0'>
