40e7561747f0530c99543668abf28814866b0403
Mayur-Tri <mayurshimpi.tri@gmail.com>
2025-02-25 09:59:17 +0530

    reviews assets update

--- src/components/landing/reviewsSection/index.tsx
+++ src/components/landing/reviewsSection/index.tsx
@@ -9,25 +9,25 @@ const reviews = [
         title: "We have been farming for 35-40 years. Usually we plant rice, cotton, and chilli. Most of the time we plant chilli crop. Last year, we planted a 100 packets of chilli seeds. But due to untimely rains, our entire farm was destroyed. That's why last year we bought a Kheyti greenhouse.",
         author: "Paadi Jyothi",
         text: "We planted 70 packets of chilli seeds inside, and the growth is very good. We also planted 10 packets of chilli seeds outside, but those were damaged. Kheyti's services are also very good, they come once a week, and are available on phone, too. If I have any doubts, I send videos them of the crops and they advise me with a voice message on what to apply. The greenhouse has been very helpful to us.",
-        img: "/assets/reviews/review-1-min.png"
+        img: "/assets/reviews/img-1.png"
     },
     {
         title: "We have been farming for 35-40 years. Usually we plant rice, cotton, and chilli. Most of the time we plant chilli crop. Last year, we planted a 100 packets of chilli seeds. But due to untimely rains, our entire farm was destroyed. That's why last year we bought a Kheyti greenhouse.",
         author: "Paadi Jyothi",
         text: "We planted 70 packets of chilli seeds inside, and the growth is very good. We also planted 10 packets of chilli seeds outside, but those were damaged. Kheyti's services are also very good, they come once a week, and are available on phone, too. If I have any doubts, I send videos them of the crops and they advise me with a voice message on what to apply. The greenhouse has been very helpful to us.",
-        img: "/assets/reviews/review-1-min.png"
+        img: "/assets/reviews/img-2.png"
     },
     {
         title: "We have been farming for 35-40 years. Usually we plant rice, cotton, and chilli. Most of the time we plant chilli crop. Last year, we planted a 100 packets of chilli seeds. But due to untimely rains, our entire farm was destroyed. That's why last year we bought a Kheyti greenhouse.",
         author: "Paadi Jyothi",
         text: "We planted 70 packets of chilli seeds inside, and the growth is very good. We also planted 10 packets of chilli seeds outside, but those were damaged. Kheyti's services are also very good, they come once a week, and are available on phone, too. If I have any doubts, I send videos them of the crops and they advise me with a voice message on what to apply. The greenhouse has been very helpful to us.",
-        img: "/assets/reviews/review-1-min.png"
+        img: "/assets/reviews/img-3.png"
     },
     {
         title: "We have been farming for 35-40 years. Usually we plant rice, cotton, and chilli. Most of the time we plant chilli crop. Last year, we planted a 100 packets of chilli seeds. But due to untimely rains, our entire farm was destroyed. That's why last year we bought a Kheyti greenhouse.",
         author: "Paadi Jyothi",
         text: "We planted 70 packets of chilli seeds inside, and the growth is very good. We also planted 10 packets of chilli seeds outside, but those were damaged. Kheyti's services are also very good, they come once a week, and are available on phone, too. If I have any doubts, I send videos them of the crops and they advise me with a voice message on what to apply. The greenhouse has been very helpful to us.",
-        img: "/assets/reviews/review-1-min.png"
+        img: "/assets/reviews/img-4.png"
     },
 ]
 
@@ -51,8 +51,8 @@ const ReviewsSlider = () => {
                 {reviews.map((review, index) => (
                     <SwiperSlide key={index}>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 bg-white rounded-lg shadow-sm border border-gray300 w-full ">
-                            <div>
-                                <img src={review.img} className="h-[200px] lg:h-full rounded-lg" alt="kheyti review" />
+                            <div className="relative h-[300px] lg:h-[350px] bg-[url(/assets/reviews/review-1-min.png)] bg-no-repeat bg-cover">
+                                <img src={review.img} className={`absolute bottom-0 h-full lg:h-full rounded-lg ${index === 0 || index === 3 ? " left-10 lg:left-12": " left-8 lg:left-8"}`} alt="kheyti review" />
                             </div>
                             <div className="lg:p-5 py-10 lg:py-16 relative w-full lg:w-[80%]">
                                 <img src="/assets/reviews/icon.png" className="w-20 h-16 absolute top-2 lg:top-10 lg:left-[-30px]" alt="kheyti review" />
